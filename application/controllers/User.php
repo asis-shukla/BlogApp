f7b8271d60a67a84d768e19370b5b3a33171c882
Ashish Shukla <ashishfun008@gmail.com>
2019-03-08 08:43:06 +0530

    initial commit

---
+++ application/controllers/User.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class User extends MY_Controller{
+
+    public function index(){
+        echo "This is index function of the user";
+    }
+}
+
+?>
