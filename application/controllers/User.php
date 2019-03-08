9efe7cb4259e79355e84526bb7b3c1d6f7250bc0
Ashish Shukla <ashishfun008@gmail.com>
2019-03-08 10:03:36 +0530

    Add front end view section along with bootstrap, js and jquery

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,7 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class User extends MY_Controller{
 
     public function index(){
-        echo "This is index function of the user";
+        // echo "This is index function of the user";
+        // $this->load->helper('html');
+        $this->load->view('public/articles_list');
+        
     }
 }
 
